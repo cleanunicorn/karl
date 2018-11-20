a8afecde6fb5136d483d64a5279b8359ee6b4ee3
Daniel Luca <lucadanielcostin@gmail.com>
2018-11-20 12:03:28 +0200

    Initialize web3 and send to Karl

--- karl/interfaces/cli.py
+++ karl/interfaces/cli.py
@@ -1,6 +1,8 @@
 import argparse
+import re
 from mythril.mythril import Mythril
 from karl.karl import Karl
+from web3 import Web3
 
 
 def main():
@@ -12,7 +14,7 @@ def main():
     rpc.add_argument(
         "--rpc",
         help="Custom RPC settings",
-        metavar="HOST:PORT / ganache / infura-[network_name]",
+        metavar="HOST:PORT / ganache / infura-[mainnet, rinkeby, kovan, ropsten]",
     )
     rpc.add_argument(
         "--rpctls", type=bool, default=False, help="RPC connection over TLS"
@@ -21,20 +23,45 @@ def main():
     args = parser.parse_args()
 
     try:
+        # Initialize Mythril
         myth = Mythril(
             onchain_storage_access=True,
             enable_online_lookup=True
         )
         myth.set_api_rpc(rpc=args.rpc, rpctls=args.rpctls)
 
+        # Initialize Web3 client
+        web3rpc = None
+        if args.rpc == "ganache":
+            web3rpc = "http://127.0.0.1:8545"
+        else:
+            m = re.match(r"infura-(.*)", args.rpc)
+            if m and m.group(1) in ["mainnet", "rinkeby", "kovan", "ropsten"]:
+                web3rpc = ("https://" + m.group(1) + ".infura.io")
+            else:
+                try:
+                    host, port = args.rpc.split(":")
+                    if args.rpctls:
+                        web3rpc = "https://" + host + ":" + port
+                    else:
+                        web3rpc = "http://" + host + ":" + port
+                except ValueError:
+                    raise Exception("Invalid RPC argument provided {}, use 'ganache', 'infura-[mainnet, rinkeby, kovan, ropsten]' or HOST:PORT".format(args.rpc))
+        
+        if web3rpc is None:
+            raise Exception("Invalid RPC argument provided {}, use 'ganache', 'infura-[mainnet, rinkeby, kovan, ropsten]' or HOST:PORT".format(args.rpc))
+
+        web3 = Web3(Web3.HTTPProvider(web3rpc, request_kwargs={'timeout': 60}))
+
         karl = Karl(
             mythril=myth,
+            web3=web3,
         )
 
         karl.run()
 
     except Exception as e:
-        print("Exception ", e)
+        print("[CLI] Exception:", e)
 
 
 if __name__ == "__main__":
--- karl/karl.py
+++ karl/karl.py
@@ -4,31 +4,52 @@ class Karl:
     """
 
     mythril = None
+    web3 = None
+    blockNumber = None
 
     def __init__(self,
-        mythril=None
+        mythril=None,
+        web3=None,
+        blockNumber=None,
     ):
         """
             Provide the initialized Mythril object
         """
         if mythril is None:
-            print("Error, no mythril")
+            raise(Exception("Must provide a valid Mythril initialized interface"))
+
+        if web3 is None:
+            raise (Exception("Must provide a valid web3 initialized interface"))
+            
+        if blockNumber is None:
+            self.blockNumber = web3.eth.blockNumber
 
         self.mythril = mythril
+        self.web3 = web3
 
     def run(self):
-        address = "0xa0af62406d044e45cb9cb5eb0a04340d7395738d"
-        self.mythril.load_from_address(address)
-        report = self.mythril.fire_lasers(
-            strategy='dfs',
-            modules=['ether_thief', 'suicide'],
-            address=address,
-            execution_timeout=45,
-            create_timeout=10,
-            max_depth=22,
-            transaction_count=2,
-            verbose_report=True,
-        )
+        print("Running")
 
-        print("Found {} issues".format(len(report.issues)))
-        print(report.as_json())
+        try:
+            block = self.web3.eth.getBlock(
+                self.blockNumber,
+                full_transactions=True,
+            )
+            print(block)
+            # address = "0x74FC6891EF3c2B4D22F594FDD9DeA5c9F1a123a9"
+            # self.mythril.load_from_address(address)
+            # report = self.mythril.fire_lasers(
+            #     strategy='dfs',
+            #     modules=['ether_thief', 'suicide'],
+            #     address=address,
+            #     execution_timeout=45,
+            #     create_timeout=10,
+            #     max_depth=22,
+            #     transaction_count=2,
+            #     verbose_report=True,
+            # )
+
+            # print("Found {} issues".format(len(report.issues)))
+            # print(report.as_json())
+        except Exception as e:
+            print("[Karl] Exception:", e)
