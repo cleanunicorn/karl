9e008d2b5507f41e33d283fc46e8e4a816abff5f
Daniel Luca <lucadanielcostin@gmail.com>
2018-11-21 12:31:15 +0200

    Fix long description file check

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import os
 
 setup(
     name="karl",
-    version="0.2",
+    version="0.2.1",
     packages=["karl"],
     license="MIT",
     author="Daniel Luca",
     author_email="daniel.luca@mythril.ai",
-    long_description=read_file("Readme.md") if os.path.isfile("README.md") else "",
+    long_description=read_file("Readme.md") if os.path.isfile("Readme.md") else "",
     long_description_content_type="text/markdown",
     credits="Bernhardt Muller",
 )
