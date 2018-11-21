e785227b34e5ea469a3431a89d6188b68ff09ef1
Daniel Luca <lucadanielcostin@gmail.com>
2018-11-21 13:46:51 +0200

    Seems to work now

--- karl/__main__.py
+++ karl/__main__.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
 from karl.interfaces.cli import main
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 import os
+
+def read_file(fname):
+    """
+    return file contents
+    :param fname: path relative to setup.py
+    :return: file contents
+    """
+    with open(os.path.join(os.path.dirname(__file__), fname), "r") as fd:
+        return fd.read()
 
 setup(
     name="karl",
-    version="0.2.2",
-    packages=["karl"],
+    version="0.2.3",
     license="MIT",
     author="Daniel Luca",
-    author_email="daniel.luca@mythril.ai",
-    long_description=open("Readme.md", "r").read() if os.path.isfile("Readme.md") else "",
+    author_email="daniel.luca@consensys.net",
+    long_description=read_file("Readme.md") if os.path.isfile("Readme.md") else "",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
     long_description_content_type="text/markdown",
+    entry_points={"console_scripts": ["karl=karl.interfaces.cli:main"]},
     credits="Bernhardt Muller",
 )
