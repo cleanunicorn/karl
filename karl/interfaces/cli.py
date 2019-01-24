7595b76722bd3f9735d81e59820fa6c79ce4736f
Daniel Luca <lucadanielcostin@gmail.com>
2019-01-24 14:14:20 +0200

    Merge verbosity-info

--- karl/interfaces/cli.py
+++ karl/interfaces/cli.py
@@ -41,7 +41,8 @@ def main():
     # Verbosity
     verbosity = parser.add_argument_group("Verbosity")
     verbosity.add_argument(
-        "--verbose", "-v", action="count", help="Set verbosity level"
+        "--verbose", "-v", action="count", help="Set verbose",
+        default=4  # logging.INFO
     )
 
     # Parse cli args
