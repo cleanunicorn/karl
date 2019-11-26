f907747adf746f674d94bae944fc0b7aa74f8240
Luca Daniel <lucadanielcostin@gmail.com>
2019-11-26 13:03:02 +0200

    Reresign the install process: (#78)
    * Reresign the install process:
    - Copy signatures.db if not exists
    - Add ubuntu.sh test script for docker testing
    
    * Fixing style errors.
    
    * Reformat with black.
    
    * Check karl starts after the setup script.

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from karl.version import __version__
 from setuptools.command.install import install
 import sys
 import os
+import shutil
+import pathlib
+import atexit
 
 
 def read_file(fname):
@@ -30,6 +33,44 @@ class VerifyVersionCommand(install):
             sys.exit(info)
 
 
+def copySignatures():
+    """Check if signatures.db is already installed and copy it in the home dir if not."""
+    mythril_path = os.path.join(pathlib.Path.home(), ".mythril")
+    signatures_path = os.path.join(mythril_path, "signatures.db")
+    if not os.path.exists(mythril_path):
+        print("Mythril db folder does not exist, creating...")
+        try:
+            os.mkdir(mythril_path)
+        except OSError:
+            print("Could not create Mythril db folder")
+            print("Manually create ~/.mythril/signatures.db")
+
+    if not os.path.exists(signatures_path):
+        print("Signatures does not exist, creating...")
+        try:
+            shutil.copyfile("./signatures/signatures.db", signatures_path)
+        except Exception as e:
+            print(sys.exc_info()[0])
+            print(type(e))  # the exception instance
+            print(e.args)  # arguments stored in .args
+            print(e)
+
+            print(
+                "Could not copy signatures.db file to {signatures_dest}".format(
+                    signatures_dest=signatures_path
+                )
+            )
+            print("Manually create ~/.mythril/signatures.db")
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def __init__(self, *args, **kwargs):
+        super(PostInstallCommand, self).__init__(*args, **kwargs)
+        atexit.register(copySignatures)
+
+
 setup(
     name="karl",
     version=__version__,
@@ -37,10 +78,12 @@ setup(
     author="Daniel Luca",
     author_email="daniel.luca@consensys.net",
     long_description=read_file("Readme.md") if os.path.isfile("Readme.md") else "",
-    packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    install_requires=read_file("requirements.txt").split("\n"),
     long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    package_data={"karl": ["signatures/signatures.db"]},
+    include_package_data=True,
+    install_requires=read_file("requirements.txt").split("\n"),
     entry_points={"console_scripts": ["karl=karl.interfaces.cli:main"]},
     credits=["Bernhard Mueller <bernhard.mueller@mithril.ai>"],
-    cmdclass={"verify": VerifyVersionCommand},
+    cmdclass={"verify": VerifyVersionCommand, "install": PostInstallCommand},
 )
