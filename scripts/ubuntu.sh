515d46ece2395e93037270aad7397549d9b8def0
Mark R <13649452+mrudnitsky@users.noreply.github.com>
2022-06-21 16:00:39 -0400

    QOL: integrate Ganache and update URL
    QOL changes

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -8,4 +8,10 @@ pip3 install maturin
 pip3 install -e .
 karl --help
 
-karl --rpc https://mainnet.infura.io/ --block=9003035
+# Start Ganache locally, catch all errors and discard them because ganache isn't a requirement for Karl to work
+./ganache &>/dev/null &
+
+# You'll have to create an account on Infura for this, but it's free to use their APIs
+# Replace with your given API URL
+rpcurl=https://mainnet.infura.io/
+karl --rpc $rpcurl --block=15003900
